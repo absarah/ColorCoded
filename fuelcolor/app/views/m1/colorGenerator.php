bef04ef6373bc704315e34054a2cc968ffa42e76
elitadanilyuk <elitadanilyuk@gmail.com>
2022-03-30 17:23:02 -0600

    test fi

--- fuelcolor/app/views/m1/colorGenerator.php
+++ fuelcolor/app/views/m1/colorGenerator.php
@@ -22,33 +22,33 @@
 	                $table = '';
                     $color_choices = array('red', 'orange', 'yellow', 'green', 'blue', 'purple', 'grey', 'brown', 'black', 'teal');
 	                if (isset($_GET['colors'])){
-                            $table .= '<table border="2">';
-                            for ($i = 0; $i < $_GET['colors']; $i++) {
-                                $table .= '<tr>';
-                                for ($j = 0; $j < 2; $j++) {
-                                    if($j % 2 == 0){
-                                        $table .= '<td width="20%"><select class="color_picker" name="color_picker" id="color_picker">
-                                        <option id = "0" value="blank"> </option>
-                                        <option id = "1" value="red" style="background-color: red;">Red</option>
-                                        <option id = "2" value="orange" style="background-color: orange;">Orange</option>
-                                        <option id = "3" value="yellow" style="background-color: yellow;">Yellow</option>
-                                        <option id = "4" value="green" style="background-color: green;">Green</option>
-                                        <option id = "5" value="blue" style="background-color: blue;">Blue</option>
-                                        <option id = "6" value="purple" style="background-color: purple;">Purple</option>
-                                        <option id = "7" valued="grey" style="background-color: grey;">Grey</option>
-                                        <option id = "8" value="brown" style="background-color: brown;">Brown</option>
-                                        <option id = "9" value="black" style="background-color: black;">Black</option>
-                                        <option id = "10" value="teal" style="background-color: teal;">Teal</option>
-                                        </select></td>';
-                                    }
-                                    else{
-                                        $table .= '<td width="80%">&nbsp;</td>';
-                                    }
-                                    
+                        $table .= '<table border="2">';
+                        for ($i = 0; $i < $_GET['colors']; $i++) {
+                            $table .= '<tr>';
+                            for ($j = 0; $j < 2; $j++) {
+                                if($j % 2 == 0){
+                                    $table .= '<td width="20%"><select class="color_picker" name="color_picker" id="color_picker">
+                                    <option id = "0" value="blank"> </option>
+                                    <option id = "1" value="red" style="background-color: red;">Red</option>
+                                    <option id = "2" value="orange" style="background-color: orange;">Orange</option>
+                                    <option id = "3" value="yellow" style="background-color: yellow;">Yellow</option>
+                                    <option id = "4" value="green" style="background-color: green;">Green</option>
+                                    <option id = "5" value="blue" style="background-color: blue;">Blue</option>
+                                    <option id = "6" value="purple" style="background-color: purple;">Purple</option>
+                                    <option id = "7" valued="grey" style="background-color: grey;">Grey</option>
+                                    <option id = "8" value="brown" style="background-color: brown;">Brown</option>
+                                    <option id = "9" value="black" style="background-color: black;">Black</option>
+                                    <option id = "10" value="teal" style="background-color: teal;">Teal</option>
+                                    </select></td>';
                                 }
-                                $table .= '</tr>';
+                                else{
+                                    $table .= '<td width="80%">&nbsp;</td>';
+                                }
+                                
                             }
-                            $table .= '</table>';
+                            $table .= '</tr>';
+                        }
+                        $table .= '</table>';
 	                }
                 ?>
                 <br/>
